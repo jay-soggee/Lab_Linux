a811acffb00b1e6bc1640a3226a7240bfcd1ecc5
jay_bear <jaesuki98@gmail.com>
2022-09-29 11:30:18 +0900

    init linux lab. repo

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "test.h"
+
+int main() {
+    printf("make.c : ");
+    test();
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test.h"
+
+void test() {
+    printf("Hello Make!");
+}
