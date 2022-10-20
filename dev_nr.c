a41a12ce79925b8b7a32109aee292885410a20e0
jay_bear <jaesuki98@gmail.com>
2022-10-20 12:55:59 +0900

    asjidfbgijk

---
+++ dev_nr.c
@@ -0,0 +1,9 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+
+/* meta info */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Johannes 4 GNU/Linux");
+MODULE_DESCRIPTION("Registers a device nr. and implement some callback functions");
+
